7f903ee3c817a9e3fb2e8d10be563b4a011735f0
jteib <27736224+jteib@users.noreply.github.com>
2019-12-15 18:27:00 -0500

    initial commit

---
+++ playground/custom-functions/cf.js
@@ -0,0 +1,33 @@
+// Function Definition
+function calculateBill(billAmount, taxRate = 0.14, tipRate = 0) {
+  console.log('Running Calculate Bill!!');
+  let total = billAmount + billAmount * taxRate + billAmount * tipRate;
+  const pennies = total * 100;
+  total = Math.round(pennies) / 100;
+
+  return total;
+}
+
+// Function Call. Or **Run**
+const wesTotal = 189.99;
+const wesTaxRate = 0.06;
+const myTotal = calculateBill(wesTotal, wesTaxRate);
+console.log(myTotal);
+
+// Function Definition
+function sayHiTo(firstName) {
+  return `Hello ${firstName}`;
+}
+
+const greeting = sayHiTo('Wes');
+console.log(greeting);
+
+function doctorize(name) {
+  return `Dr. ${name}`;
+}
+
+function yell(name = 'Silly Goose') {
+  return `HEY ${name.toUpperCase()}`;
+}
+
+const myBill4 = calculateBill(100, undefined, 0.2);
